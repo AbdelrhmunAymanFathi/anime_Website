ef666a704851c46ffa8a5a962840d8e6648b82f0
AbdelrhmunAymanFathi <127216417+AbdelrhmunAymanFathi@users.noreply.github.com>
2024-02-12 22:17:56 +0200

    my changes

---
+++ main.js
@@ -0,0 +1,14 @@
+let btn = document.getElementById('btn')
+let mobile_menu = document.getElementById('mobile_menu')
+let body = document.getElementById('body')
+
+function myFunction() {
+    if(mobile_menu.style.display === "none")
+    {
+        mobile_menu.style.display = "block"
+        body.style.overflow = "hidden"
+    }else
+    {
+        mobile_menu.style.display = "none"
+    }
+    }
