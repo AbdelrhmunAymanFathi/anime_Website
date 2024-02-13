34d90867b14fd2663555bb9528b258d6c76a8a0d
AbdelrhmunAymanFathi <127216417+AbdelrhmunAymanFathi@users.noreply.github.com>
2024-02-13 02:05:55 +0200

    my changes

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ let btn = document.getElementById('btn')
 let mobile_menu = document.getElementById('mobile_menu')
 let body = document.getElementById('body')
 
-function myFunction() {
+function myFunction() 
+{
     if(mobile_menu.style.display === "none")
     {
         mobile_menu.style.display = "block"
@@ -12,4 +13,4 @@ function myFunction() {
         mobile_menu.style.display = "none"
         body.style.overflow = "auto"
     }
-    }
+}
